c89c75e960a7920ba3309f129caed4eae2854715
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2025-01-03 01:04:16 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import TokenInfoView from "./components/token-info-view";
 import { getTokenDataISR } from "./fetchers/get-token-data-isr";
 
 const HomePage = async () => {
-  const tokenData = await getTokenDataISR("eth", "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2");
+  const tokenData = await getTokenDataISR("tswap", "0xAbE78f9f591F8802955155abB8B2C2dB19F4adF7");
 
   if (!tokenData) {
     return null;
